2f2336a1ff221618a8ed2dc3ae9d4fc10263ae3f
DouglasMonteles <silvadouglas328@gmail.com>
2025-04-27 18:08:30 -0300

    :tada: init springboot project
    Change-Id: I64db8c231a04b5ebc58452e8cd0e21ee3b669ee4

---
+++ src/main/java/com/poc/springboot_jade/SpringbootJadeApplication.java
@@ -0,0 +1,13 @@
+package com.poc.springboot_jade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/springboot_jade/SpringbootJadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.springboot_jade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
